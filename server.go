aef38cbc9de8db05c5ca6b5f458b729fc24573f1
Ilya Gorsky <iagorsky@yandex.ru>
2017-05-03 01:48:12 +0300

    api /all/ route

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"time"
@@ -20,27 +22,55 @@ type Timeline struct {
 	Data []Datapoint `json:"data"`
 }
 
-func ApiHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	a := []byte{}
-
-	if id == "all" {
-		w.Write(a)
-	} else if _, err := strconv.Atoi(id); err != nil {
-		w.Write(a)
-	} else {
-		w.Write(a)
-	}
-}
+var ChunkReader func(...interface{}) (*sql.Rows, error)
 
 func startServer() {
 	router := mux.NewRouter()
 	fs := http.FileServer(http.Dir("static"))
+
+	var closeReader func() error
+	closeReader, ChunkReader = CreateChunkReader()
+	defer closeReader()
 
 	router.Handle("/", fs)
 	router.HandleFunc("/api/{id}", ApiHandler)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":3000", nil)
+}
+
+func ApiHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	a := []byte{}
+
+	if id == "all" {
+		timeline := Timeline{}
+		rows, err := ChunkReader()
+		defer rows.Close()
+		logErr(err)
+
+		for rows.Next() {
+			datapoint := Datapoint{}
+
+			err := rows.Scan(
+				&datapoint.ID,
+				&datapoint.Temperature,
+				&datapoint.Humidity,
+				&datapoint.Pressure,
+				&datapoint.Timestamp)
+
+			logErr(err)
+			timeline.Data = append(timeline.Data, datapoint)
+		}
+
+		j, err := json.Marshal(timeline)
+		logErr(err)
+
+		w.Write(j)
+	} else if _, err := strconv.Atoi(id); err != nil {
+		w.Write(a)
+	} else {
+		w.Write(a)
+	}
 }
