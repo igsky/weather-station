522dbf753c4f9dcb3ef8c46988261e6a18b4588f
igsky <18455809+igsky@users.noreply.github.com>
2018-05-20 18:29:08 +0300

    Moving SensorReading

--- sensor_reader.go
+++ sensor_reader.go
@@ -1,11 +1,5 @@
 package main
 
-type SensorReading struct {
-	Temperature float32
-	Humidity    float32
-	Pressure    float32
-}
-
 // SensorReader is an interface to read data from sensors
 type SensorReader interface {
 	Read() (SensorReading, error)
---
+++ sensor_reading.go
@@ -0,0 +1,8 @@
+package main
+
+// SensorReading represents single reading from temperature, humidity and pressure sensors
+type SensorReading struct {
+	Temperature float32
+	Humidity    float32
+	Pressure    float32
+}
